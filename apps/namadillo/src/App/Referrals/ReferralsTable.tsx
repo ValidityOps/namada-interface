d0e6511868d5eebce1bb2c5111bd6b990c128f99
neocybereth <sean@arrowburn.co.nz>
2025-04-14 19:06:12 +1200

    fix: upgrade

--- apps/namadillo/src/App/Referrals/ReferralsTable.tsx
+++ apps/namadillo/src/App/Referrals/ReferralsTable.tsx
@@ -169,7 +169,7 @@ export const ReferralsTable = ({
         // Get rewards for each epoch from start_epoch to current epoch
         for (
           let epoch = referral.start_epoch;
-          epoch < chainStatus.epoch + 1;
+          epoch < chainStatus.epoch;
           epoch++
         ) {
           try {
